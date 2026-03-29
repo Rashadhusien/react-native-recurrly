61d4fa87908666e86f81fd93f5ec433080af88c4
rashad <rashadhusein440@gmail.com>
2026-03-29 19:29:37 +0200

    implement fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,40 +6,25 @@ import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-background p-5">
-      <Text className="text-xl font-bold text-accent">
-        Welcome to Nativewind!
-      </Text>
+    <SafeAreaView className="flex-1  bg-background p-5">
+      <Text className="text-7xl  text-accent font-sans-extrabold ">Home</Text>
       <Link
         href="/onboarding"
-        className="mt-4 rounded bg-primary p-4 text-center text-white"
+        className="mt-4 rounded bg-primary p-4 font-sans-bold text-center text-white"
       >
         <Text>Go to onboarding</Text>
       </Link>
       <Link
         href="/(auth)/sign-in"
-        className="mt-4 rounded bg-primary p-4 text-center text-white"
+        className="mt-4 rounded bg-primary p-4 font-sans-bold text-center text-white"
       >
         <Text>Go to sign in</Text>
       </Link>
       <Link
         href="/(auth)/sign-up"
-        className="mt-4 rounded bg-primary p-4 text-center text-white"
+        className="mt-4 rounded bg-primary p-4 font-sans-bold text-center text-white"
       >
         <Text>Go to sign up</Text>
-      </Link>
-
-      <Link
-        href="/subscriptions/spotify"
-        className="mt-4 rounded bg-primary p-4 text-center text-white"
-      >
-        <Text>Go to subscriptions</Text>
-      </Link>
-      <Link
-        href={{ pathname: "/subscriptions/[id]", params: { id: "claude" } }}
-        className="mt-4 rounded bg-primary p-4 text-center text-white"
-      >
-        <Text>Claude max subscription</Text>
       </Link>
     </SafeAreaView>
   );
